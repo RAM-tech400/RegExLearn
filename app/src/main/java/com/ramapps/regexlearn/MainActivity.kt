76e826f32feb4f03c6f2b3c153c886ebe8cd542e
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-06-26 15:40:49 +0330

    Define components in MainActivity class and adding neccessary methods

--- app/src/main/java/com/ramapps/regexlearn/MainActivity.kt
+++ app/src/main/java/com/ramapps/regexlearn/MainActivity.kt
@@ -5,16 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fragmentContainerView: FragmentContainerView
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initViews()
+        addListeners()
+    }
+
+    private fun addListeners() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, 0, systemBars.right, 0)
             insets
         }
+    }
+
+    private fun initViews() {
+        fragmentContainerView = findViewById(R.id.main_fragment_container_view)
+        bottomNavigationView = findViewById(R.id.main_bottom_navigation_view)
     }
 }
