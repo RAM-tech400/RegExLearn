bae4cbeafef9e8771c0634670c342590429c7b5b
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-06-27 09:52:35 +0330

    Some Implement In Utils.kt

--- app/src/main/java/com/ramapps/regexlearn/Utils.kt
+++ app/src/main/java/com/ramapps/regexlearn/Utils.kt
@@ -1,16 +1,79 @@
 package com.ramapps.regexlearn
 
-import androidx.annotation.NonNull
+import android.content.res.Resources
+import android.util.Log
 import org.json.JSONArray
 import org.json.JSONObject
+import java.io.StringWriter
 
 class Utils {
-    fun getLessonsTitleFromDataJson(@NonNull jsonArrayLessons: JSONArray) : List<String> {
-        var titlesArrayList : ArrayList<String>
+    companion object {
+        const val TAG = "Utils"
+    }
 
-        for(i in 0..<jsonArrayLessons.length()){
-            val jsonLesson = jsonArrayLessons.getJSONObject(i)
-            titlesArrayList.add()
+    fun getJSONArrayFromRaw(rawResources: Resources, rawJsonId: Int): JSONArray {
+        Log.v(TAG, "getJSONArrayFromRaw()")
+
+        val inputStream = rawResources.openRawResource(rawJsonId)
+        val writer = StringWriter()
+        while (true) {
+            val n = inputStream.read()
+
+            if (n < 0) break
+
+            writer.write(n)
         }
+
+        val stringJSON = writer.toString()
+
+        Log.v(TAG, "Closing open streams for making free-up resources...")
+        inputStream.close()
+        writer.close()
+
+        return JSONArray(stringJSON)
+    }
+
+    fun getJSONObjectFromRaw(rawResources: Resources, rawJsonId: Int): JSONObject {
+        Log.v(TAG, "getJSONObjectFromRaw()")
+
+        val inputStream = rawResources.openRawResource(rawJsonId)
+        val writer = StringWriter()
+
+        while (true) {
+            val n = inputStream.read()
+
+            if (n < 0) break
+
+            writer.write(n)
+        }
+
+        val stringJSON = writer.toString()
+
+        Log.v(TAG, "Closing open streams for making free-up resources...")
+        inputStream.close()
+        writer.close()
+
+        return JSONObject(stringJSON)
+    }
+
+    fun getLessonsTitleFromDataJson(
+        jsonArrayLessons: JSONArray,
+        jsonLocalizationLessonValues: JSONObject
+    ): List<String> {
+        Log.v(TAG, "getLessonsTitleFromDataJson")
+        val titlesArrayList = ArrayList<String>()
+
+        for (i in 0..<jsonArrayLessons.length()) {
+            val jsonLesson = jsonArrayLessons.getJSONObject(i)
+            titlesArrayList.add(
+                jsonLocalizationLessonValues.getString(
+                    jsonLesson.getString(
+                        GlobalVariables.LESSON_JSON_KEY_TITLE
+                    )
+                )
+            )
+        }
+
+        return titlesArrayList
     }
 }
