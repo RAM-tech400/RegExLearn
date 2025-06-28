ecef48288bad7ce4f174060d7d714f6e5d54012f
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-06-28 16:53:49 +0330

    Rename getLessonsSelectionView to makeLessonsSelectionView and put it into BottomSheetClass for fix some crashes

--- app/src/main/java/com/ramapps/regexlearn/LearningFragment.kt
+++ app/src/main/java/com/ramapps/regexlearn/LearningFragment.kt
@@ -17,6 +17,8 @@ import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.widget.Toolbar.OnMenuItemClickListener
 import androidx.core.view.updateMarginsRelative
+import androidx.core.widget.NestedScrollView
+import androidx.core.widget.NestedScrollView.OnScrollChangeListener
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -28,10 +30,67 @@ class LearningFragment : Fragment() {
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
             val dialog = BottomSheetDialog(requireContext())
 
-            dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
-            dialog.setContentView(LearningFragment().getLessonSelectionView(requireContext()))
+            dialog.setContentView(getLessonSelectionView())
+            dialog.behavior.isShouldRemoveExpandedCorners = true
 
             return dialog
+        }
+
+        @SuppressLint("InflateParams")
+        fun getLessonSelectionView() : View {
+            val lessonsTitle = Utils().getLessonsTitleFromDataJson(
+                Utils().getJSONArrayFromRaw(requireContext().resources, R.raw.lessons_data),
+                Utils().getJSONObjectFromRaw(requireContext().resources, R.raw.lessons_localization_data),
+            )
+
+            val scrollView = ScrollView(requireContext())
+            scrollView.scrollBarSize = 0
+
+            val layout = LinearLayout(requireContext())
+            layout.orientation = LinearLayout.VERTICAL
+
+            scrollView.removeAllViews()
+            layout.removeAllViews()
+            scrollView.setOnScrollChangeListener(object : View.OnScrollChangeListener {
+                override fun onScrollChange(
+                    v: View?,
+                    scrollX: Int,
+                    scrollY: Int,
+                    oldScrollX: Int,
+                    oldScrollY: Int
+                ) {
+                    (dialog as BottomSheetDialog).behavior.isDraggable = (scrollY == 0)
+                }
+            })
+
+            val titleTextView = TextView(requireContext())
+            titleTextView.text = requireContext().getString(R.string.select_lesson)
+            titleTextView.textSize = 22f
+            val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+            layoutParams.updateMarginsRelative(24, 24, 0, 16)
+            titleTextView.layoutParams = layoutParams
+            layout.addView(titleTextView)
+
+            lessonsTitle.forEach { t ->
+                val v = LayoutInflater.from(requireContext()).inflate(R.layout.item_view_lesson, null)
+                val lessonTitleTextView = v.findViewById<TextView>(R.id.item_view_lesson_text_view_title)
+                v.findViewById<ImageView>(R.id.item_view_lesson_image_view_state)
+                val parentLayout = v.findViewById<LinearLayout>(R.id.item_view_lesson_parent)
+
+                lessonTitleTextView.text = Utils().stylingFormattedText(t)
+
+                parentLayout.setOnClickListener{ _ ->
+                    Toast.makeText(requireContext(), "Lesson <${t}> selected", Toast.LENGTH_SHORT).show()
+                    requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
+                }
+
+                v.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                layout.addView(v)
+            }
+
+            scrollView.addView(layout)
+
+            return scrollView
         }
 
         companion object {
@@ -69,51 +128,6 @@ class LearningFragment : Fragment() {
 
     private fun initViews(view: View) {
         toolbar = view.findViewById(R.id.learning_fragment_toolbar)
-    }
-
-    @SuppressLint("InflateParams")
-    fun getLessonSelectionView(context: Context) : View {
-        val lessonsTitle = Utils().getLessonsTitleFromDataJson(
-            Utils().getJSONArrayFromRaw(context.resources, R.raw.lessons_data),
-            Utils().getJSONObjectFromRaw(context.resources, R.raw.lessons_localization_data),
-        )
-
-        val scrollView = ScrollView(context)
-        scrollView.scrollBarSize = 0
-
-        val layout = LinearLayout(context)
-        layout.orientation = LinearLayout.VERTICAL
-
-        scrollView.removeAllViews()
-        layout.removeAllViews()
-
-        val titleTextView = TextView(context)
-        titleTextView.text = context.getString(R.string.select_lesson)
-        titleTextView.textSize = 22f
-        val layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        layoutParams.updateMarginsRelative(24, 24, 0, 16)
-        titleTextView.layoutParams = layoutParams
-        layout.addView(titleTextView)
-
-        lessonsTitle.forEach { t ->
-            val v = LayoutInflater.from(context).inflate(R.layout.item_view_lesson, null)
-            val lessonTitleTextView = v.findViewById<TextView>(R.id.item_view_lesson_text_view_title)
-            v.findViewById<ImageView>(R.id.item_view_lesson_image_view_state)
-            val parentLayout = v.findViewById<LinearLayout>(R.id.item_view_lesson_parent)
-
-            lessonTitleTextView.text = Utils().stylingFormattedText(t)
-
-            parentLayout.setOnClickListener{ _ ->
-                Toast.makeText(context, "Lesson <${t}> selected", Toast.LENGTH_SHORT).show()
-            }
-
-            v.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-            layout.addView(v)
-        }
-
-        scrollView.addView(layout)
-
-        return scrollView
     }
 
     companion object {
