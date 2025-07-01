85bee8887aee0f64f77ffe36b20249efd2a752a8
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-01 10:47:55 +0330

    Create RegexUtils class for delveloping utitlity methods to work with Regex. Then implement applyStyleToString() and getFlags().

---
+++ app/src/main/java/com/ramapps/regexlearn/RegexUtils.kt
@@ -0,0 +1,41 @@
+package com.ramapps.regexlearn
+
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.BackgroundColorSpan
+import android.util.Log
+import com.google.android.material.color.MaterialColors
+import java.util.regex.Pattern
+
+class RegexUtils {
+    companion object {
+        const val TAG = "RegexUtils"
+    }
+
+
+    fun applyStyleToString(regex: Regex, flags: String = "", text: String) : CharSequence {
+        Log.v(TAG, "applyStyleToString($regex, $flags, ${text})")
+        val pattern = Pattern.compile(regex.pattern, getFlagsInt(flags))
+        val matcher = pattern.matcher(text)
+        val styledString = SpannableStringBuilder(text)
+        val highlightColor = MaterialColors.harmonize(0xff00ff00.toInt(), android.R.attr.colorBackground)
+
+        while (matcher.find()) {
+            styledString.setSpan(BackgroundColorSpan(highlightColor), matcher.start(), matcher.end(), SpannableString.SPAN_EXCLUSIVE_INCLUSIVE)
+        }
+
+        return styledString
+    }
+
+    fun getFlagsInt(flagsLetters: String) : Int {
+        var flags = 0
+        if (flagsLetters.contains('i', true)) {
+            flags = flags or Pattern.CASE_INSENSITIVE
+        }
+        if (flagsLetters.contains('m', true)) {
+            flags = flags or Pattern.MULTILINE
+        }
+        return flags
+    }
+}
