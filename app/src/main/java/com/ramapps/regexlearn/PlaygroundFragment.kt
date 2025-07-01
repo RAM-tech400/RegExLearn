3f2dc101bacd32d02f85876cebae9bba9ff62e97
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-01 20:23:20 +0330

    Define regex pattern input layout.

--- app/src/main/java/com/ramapps/regexlearn/PlaygroundFragment.kt
+++ app/src/main/java/com/ramapps/regexlearn/PlaygroundFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.material.chip.ChipGroup
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -14,6 +15,7 @@ class PlaygroundFragment : Fragment() {
     private lateinit var previewTextInput : TextView
     private lateinit var runFloatingActionButton : FloatingActionButton
     private lateinit var flagsChipGroup: ChipGroup
+    private lateinit var regexPatternInputsLinearLayout : LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
