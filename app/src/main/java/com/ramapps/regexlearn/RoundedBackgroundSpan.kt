f4ee3f25639edc5c92bf252bdabf630c7c8b1b68
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-02 21:38:05 +0330

    Implement a custom text style span with rounded corner and horizontal padding.

---
+++ app/src/main/java/com/ramapps/regexlearn/RoundedBackgroundSpan.kt
@@ -0,0 +1,41 @@
+package com.ramapps.regexlearn
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.text.style.ReplacementSpan
+
+class RoundedBackgroundSpan(
+    private var backgroundColor: Int,
+    private var cornerRadius: Int = 12,
+    private var paddingHorizontal: Int = 4
+) : ReplacementSpan() {
+
+    override fun getSize(paint: Paint, text: CharSequence?, start: Int, end: Int, fm: Paint.FontMetricsInt?): Int {
+        // Should return the size of span.
+        // The span size contain text with and paddings.
+        return Math.round(paint.measureText(text, start, end) + paddingHorizontal * 2)
+    }
+
+    override fun draw(canvas: Canvas, text: CharSequence?, start: Int, end: Int, x: Float, top: Int, y: Int, bottom: Int, paint: Paint) {
+        // Save original style and color to restore after drawing rounded background.
+        val originalStyle = paint.style
+        val originalColor = paint.color
+
+        val textWidth = paint.measureText(text, start, end)
+
+        val rectF = RectF(x, top.toFloat(), x + textWidth, bottom.toFloat())
+
+        paint.setColor(backgroundColor)
+        paint.style = Paint.Style.FILL
+        canvas.drawRoundRect(rectF, cornerRadius.toFloat(), cornerRadius.toFloat(), paint)
+
+        paint.style = originalStyle
+        paint.color = originalColor
+
+        if (text != null) {
+            canvas.drawText(text, start, end, x, y.toFloat(), paint)
+        }
+    }
+
+}
