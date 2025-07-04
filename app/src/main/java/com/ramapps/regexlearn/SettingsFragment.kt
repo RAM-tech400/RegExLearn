e507d0f423860743791e70a220cbcc969c66e3e0
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-04 14:33:54 +0330

    Getting dark mode state from shared preferences and show it in settings fragment.

--- app/src/main/java/com/ramapps/regexlearn/SettingsFragment.kt
+++ app/src/main/java/com/ramapps/regexlearn/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.ramapps.regexlearn
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.card.MaterialCardView
 
 class SettingsFragment : Fragment() {
@@ -13,6 +16,8 @@ class SettingsFragment : Fragment() {
     private lateinit var darkModeSettingsItemCardView : MaterialCardView
     private lateinit var contactMeSettingsItemCardView : MaterialCardView
     private lateinit var aboutAppSettingsItemCardView : MaterialCardView
+
+    private lateinit var parentView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +31,7 @@ class SettingsFragment : Fragment() {
     }
 
     private fun initViews(parentView: View) {
+        this.parentView = parentView
         languageSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_language)
         darkModeSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_night_mode)
         contactMeSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_contact_me)
@@ -51,7 +57,20 @@ class SettingsFragment : Fragment() {
     }
 
     private fun loadSettingsToShow() {
-        // Todo: Implement later.
+        loadAndShowDarkMode()
+    }
+
+    private fun loadAndShowDarkMode() {
+        val darkModeDescriptionTextView = parentView.findViewById<TextView>(R.id.settings_fragment_text_view_description_night_mode)
+        val darkModeState = requireActivity()
+            .getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
+            .getInt(GlobalVariables.PREFERENCES_SETTINGS_DARK_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+
+        when (darkModeState) {
+            AppCompatDelegate.MODE_NIGHT_NO -> darkModeDescriptionTextView.text = getString(R.string.off)
+            AppCompatDelegate.MODE_NIGHT_YES -> darkModeDescriptionTextView.text = getString(R.string.on)
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> darkModeDescriptionTextView.text = getString(R.string.system_default)
+        }
     }
 
     companion object {
