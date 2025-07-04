2436a532b49a7088cb285d59ae2629978b8c0349
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-04 10:38:21 +0330

    Define components and add listeners in SettingsFragment.kt

--- app/src/main/java/com/ramapps/regexlearn/SettingsFragment.kt
+++ app/src/main/java/com/ramapps/regexlearn/SettingsFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.card.MaterialCardView
 
 class SettingsFragment : Fragment() {
+
+    private lateinit var languageSettingsItemCardView : MaterialCardView
+    private lateinit var darkModeSettingsItemCardView : MaterialCardView
+    private lateinit var contactMeSettingsItemCardView : MaterialCardView
+    private lateinit var aboutAppSettingsItemCardView : MaterialCardView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,11 +26,28 @@ class SettingsFragment : Fragment() {
     }
 
     private fun initViews(parentView: View) {
-        // Todo: Implement later.
+        languageSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_language)
+        darkModeSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_night_mode)
+        contactMeSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_contact_me)
+        aboutAppSettingsItemCardView = parentView.findViewById(R.id.settings_fragment_card_view_about_app)
     }
 
     private fun addListeners() {
-        // Todo: Implement later.
+        languageSettingsItemCardView.setOnClickListener{
+            // Todo: Implement later.
+        }
+
+        darkModeSettingsItemCardView.setOnClickListener{
+            // Todo: Implement later.
+        }
+
+        contactMeSettingsItemCardView.setOnClickListener{
+            // Todo: Implement later.
+        }
+
+        aboutAppSettingsItemCardView.setOnClickListener{
+            // Todo: Implement later.
+        }
     }
 
     private fun loadSettingsToShow() {
