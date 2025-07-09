3c4869a9b5e3b53a1a1afea690f737c27f777adf
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-09 12:59:37 +0330

    Make cleaner SettingsFragment.kt codes.

--- app/src/main/java/com/ramapps/regexlearn/fragments/SettingsFragment.kt
+++ app/src/main/java/com/ramapps/regexlearn/fragments/SettingsFragment.kt
@@ -2,39 +2,39 @@ package com.ramapps.regexlearn.fragments
 
 import android.app.Activity
 import android.app.LocaleManager
-import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.edit
 import androidx.core.os.LocaleListCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.card.MaterialCardView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.ramapps.regexlearn.GlobalVariables
+import com.ramapps.regexlearn.R
+import com.ramapps.regexlearn.activities.AboutActivity
+import com.ramapps.regexlearn.activities.MainActivity
 import java.util.Locale
 import java.util.Objects
-import androidx.core.content.edit
-import com.ramapps.regexlearn.activities.AboutActivity
-import com.ramapps.regexlearn.GlobalVariables
-import com.ramapps.regexlearn.activities.MainActivity
-import com.ramapps.regexlearn.R
 
 class SettingsFragment : Fragment() {
+    private lateinit var settingsPreferences : SharedPreferences
 
     private lateinit var languageSettingsItemCardView : MaterialCardView
     private lateinit var defaultFragmentSettingsItemCardView : MaterialCardView
     private lateinit var darkModeSettingsItemCardView : MaterialCardView
     private lateinit var contactMeSettingsItemCardView : MaterialCardView
     private lateinit var aboutAppSettingsItemCardView : MaterialCardView
-
     private lateinit var parentView: View
 
     override fun onCreateView(
@@ -42,10 +42,15 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
+        initVariables()
         initViews(view)
         addListeners()
         loadSettingsToShow()
         return view
+    }
+
+    private fun initVariables() {
+        settingsPreferences = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
     }
 
     private fun initViews(parentView: View) {
@@ -72,28 +77,27 @@ class SettingsFragment : Fragment() {
 
             val dialog = MaterialAlertDialogBuilder(requireContext())
                 .setTitle(getString(R.string.language))
-                .setSingleChoiceItems(languagesLabelList.toTypedArray(), defaultSelection, DialogInterface.OnClickListener{dialog, which ->
+                .setSingleChoiceItems(languagesLabelList.toTypedArray(), defaultSelection) { dialog, which ->
                     updateAppLanguage(languagesLabelList[which])
                     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-                        val relauanchIntent = Intent(requireContext(), MainActivity::class.java)
-                        relauanchIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(relauanchIntent)
+                        val relaunchIntent = Intent(requireContext(), MainActivity::class.java)
+                        relaunchIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                        startActivity(relaunchIntent)
                     }
                     dialog.dismiss()
                     loadAndShowLanguage()
-                })
+                }
                 .create()
             dialog.window!!.attributes.gravity = Gravity.BOTTOM
             dialog.show()
         }
 
         defaultFragmentSettingsItemCardView.setOnClickListener{
-            val prefs = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
             val bottomNavigationItemsIdList = arrayOf(
                 getString(R.string.home),
                 getString(R.string.learning),
                 getString(R.string.playground))
-            val currentDefaultFragment = prefs.getInt(
+            val currentDefaultFragment = settingsPreferences.getInt(
                 GlobalVariables.PREFERENCES_SETTINGS_DEFAULT_FRAGMENT,
                 R.id.menu_item_home
             )
@@ -104,20 +108,20 @@ class SettingsFragment : Fragment() {
             }
             val dialog = MaterialAlertDialogBuilder(requireContext())
                 .setTitle(getString(R.string.default_launch_screen))
-                .setSingleChoiceItems(bottomNavigationItemsIdList, defSelection, DialogInterface.OnClickListener{dialog, which ->
+                .setSingleChoiceItems(bottomNavigationItemsIdList, defSelection) {dialog, which ->
                     val selectedBottomNavigationItemId = when(which) {
                         1 -> R.id.menu_item_learning
                         2 -> R.id.menu_item_playground
                         else -> R.id.menu_item_home
                     }
-                    prefs.edit{
+                    settingsPreferences.edit{
                         putInt(
                             GlobalVariables.PREFERENCES_SETTINGS_DEFAULT_FRAGMENT,
                             selectedBottomNavigationItemId)
                     }
                     dialog.dismiss()
                     loadAndShowDefaultFragment()
-                })
+                }
                 .create()
             dialog.window!!.attributes.gravity = Gravity.BOTTOM
             dialog.show()
@@ -125,24 +129,25 @@ class SettingsFragment : Fragment() {
 
 
         darkModeSettingsItemCardView.setOnClickListener{
-            val prefs = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
             val darkModeLabels = arrayOf(getString(R.string.off), getString(R.string.on), getString(
                 R.string.system_default
             ))
-            val darkModeCurrentState = when (prefs.getInt(GlobalVariables.PREFERENCES_SETTINGS_DARK_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)) {
-                AppCompatDelegate.MODE_NIGHT_NO -> 0
-                AppCompatDelegate.MODE_NIGHT_YES -> 1
-                else -> 2
-            }
+            val darkModeCurrentState = when (settingsPreferences.getInt(
+                GlobalVariables.PREFERENCES_SETTINGS_DARK_MODE,
+                AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)) {
+                    AppCompatDelegate.MODE_NIGHT_NO -> 0
+                    AppCompatDelegate.MODE_NIGHT_YES -> 1
+                    else -> 2
+                }
             val dialog = MaterialAlertDialogBuilder(requireContext())
                 .setTitle(getString(R.string.dark_mode))
-                .setSingleChoiceItems(darkModeLabels, darkModeCurrentState, DialogInterface.OnClickListener{dia, which ->
+                .setSingleChoiceItems(darkModeLabels, darkModeCurrentState) {dia, which ->
                     val selectedDarkMode = when(which) {
                         0 -> AppCompatDelegate.MODE_NIGHT_NO
                         1 -> AppCompatDelegate.MODE_NIGHT_YES
                         else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
                     }
-                    prefs.edit {
+                    settingsPreferences.edit {
                         putInt(
                             GlobalVariables.PREFERENCES_SETTINGS_DARK_MODE,
                             selectedDarkMode
@@ -151,7 +156,7 @@ class SettingsFragment : Fragment() {
                     AppCompatDelegate.setDefaultNightMode(selectedDarkMode)
                     loadAndShowDarkMode()
                     dia.dismiss()
-                })
+                }
                 .create()
             dialog.window!!.attributes.gravity = Gravity.BOTTOM
             dialog.show()
@@ -169,67 +174,18 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    private fun loadAndShowDefaultFragment() {
-        val prefs = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
-        val currentBottomNavigationDefaultItemId = prefs.getInt(
-            GlobalVariables.PREFERENCES_SETTINGS_DEFAULT_FRAGMENT,
-            R.id.menu_item_home
-        )
-        val defaultBottomNavigationFragmentStateIcon = when(currentBottomNavigationDefaultItemId) {
-            R.id.menu_item_learning -> R.drawable.outline_history_edu_24
-            R.id.menu_item_playground -> R.drawable.outline_playground_24px
-            else -> R.drawable.outline_home_24
-        }
-        val defaultBottomNavigationFragmentStateImageView = parentView.findViewById<ImageView>(R.id.settings_fragment_image_view_icon_default_fragment)
-        defaultBottomNavigationFragmentStateImageView.setImageResource(defaultBottomNavigationFragmentStateIcon)
-    }
-
-    private fun updateAppLanguage(languageLabel: String) {
-        val locale = languageLabelToLocale(languageLabel)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            // Use the system integrated locale manager to manage languages and locales.
-            // This give the user the ability to change app language from system settings.
-            Log.d(TAG, "Using android system LocaleManager to manage locales.")
-            val locales: LocaleListCompat =
-                if(Objects.equals(locale.language, "")) {
-                LocaleListCompat.getEmptyLocaleList()
-            } else {
-                LocaleListCompat.create(languageLabelToLocale(languageLabel))
-            }
-            AppCompatDelegate.setApplicationLocales(locales)
-        } else {
-            // In old android versions we should manage locales by ourself.
-            // Using shared preferences to save and restore locales.
-            Log.d(TAG, "Using SharedPreferences to manage locales.")
-            val prefs = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
-            prefs.edit { putString(GlobalVariables.PREFERENCES_SETTINGS_LANGUAGE, locale.language) }
-        }
-    }
-
-    private fun languageLabelToLocale(languageLabel: String): Locale {
-        Log.d(TAG, "languageLabelToLocale($languageLabel)")
-        return when(languageLabel) {
-            getString(R.string.arabic) -> Locale("ar")
-            getString(R.string.english) -> Locale("en")
-            getString(R.string.french) -> Locale("fr")
-            getString(R.string.german) -> Locale("de")
-            getString(R.string.italian) -> Locale("it")
-            getString(R.string.korean) -> Locale("ko")
-            getString(R.string.persian) -> Locale("fa")
-            getString(R.string.russia) -> Locale("ru")
-            getString(R.string.spanish) -> Locale("es")
-            else -> Locale("")
-        }
+    private fun loadSettingsToShow() {
+        loadAndShowLanguage()
+        loadAndShowDefaultFragment()
+        loadAndShowDarkMode()
     }
 
     private fun getCurrentLocale(): Locale {
-        var currentLocale = Locale("")
-
+        val currentLocale: Locale
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             // Use the system integrated locale manager to manage languages and locales.
             // This give the user the ability to change app language from system settings.
             Log.d(TAG, "Using android system LocaleManager to manage locales.")
-            val localeManager = requireActivity().getSystemService(LocaleManager::class.java)
             val locales = AppCompatDelegate.getApplicationLocales()
             Log.d(TAG, "Application locales: $locales")
             currentLocale = Objects.requireNonNullElse(locales.get(0), Locale(""))
@@ -240,9 +196,7 @@ class SettingsFragment : Fragment() {
             val prefs = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
             currentLocale = Locale(prefs.getString(GlobalVariables.PREFERENCES_SETTINGS_LANGUAGE, "")!!)
         }
-
         Log.d(TAG, "Detected locale that used by app currently: $currentLocale")
-
         return currentLocale
     }
 
@@ -262,14 +216,29 @@ class SettingsFragment : Fragment() {
         }
     }
 
-    private fun loadSettingsToShow() {
-        loadAndShowDarkMode()
-        loadAndShowDefaultFragment()
-        loadAndShowLanguage()
+    private fun updateAppLanguage(languageLabel: String) {
+        val locale = languageLabelToLocale(languageLabel)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Use the system integrated locale manager to manage languages and locales.
+            // This give the user the ability to change app language from system settings.
+            Log.d(TAG, "Using android system LocaleManager to manage locales.")
+            val locales: LocaleListCompat =
+                if(Objects.equals(locale.language, "")) {
+                    LocaleListCompat.getEmptyLocaleList()
+                } else {
+                    LocaleListCompat.create(languageLabelToLocale(languageLabel))
+                }
+            AppCompatDelegate.setApplicationLocales(locales)
+        } else {
+            // In old android versions we should manage locales by ourself.
+            // Using shared preferences to save and restore locales.
+            Log.d(TAG, "Using SharedPreferences to manage locales.")
+            settingsPreferences.edit { putString(GlobalVariables.PREFERENCES_SETTINGS_LANGUAGE, locale.language) }
+        }
     }
 
     private fun loadAndShowLanguage() {
-        var currentLocale = Locale("")
+        val currentLocale: Locale
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             // Use the system integrated locale manager to manage languages and locales.
             // This give the user the ability to change app language from system settings.
@@ -285,23 +254,49 @@ class SettingsFragment : Fragment() {
             val prefs = requireActivity().getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
             currentLocale = Locale(prefs.getString(GlobalVariables.PREFERENCES_SETTINGS_LANGUAGE, "")!!)
         }
-
         val languageDescriptionTextView = parentView.findViewById<TextView>(R.id.settings_fragment_text_view_description_language)
         languageDescriptionTextView.text = localeToLanguageLabel(currentLocale)
     }
 
+    private fun loadAndShowDefaultFragment() {
+        val currentBottomNavigationDefaultItemId = settingsPreferences.getInt(
+            GlobalVariables.PREFERENCES_SETTINGS_DEFAULT_FRAGMENT,
+            R.id.menu_item_home
+        )
+        val defaultBottomNavigationFragmentStateIcon = when(currentBottomNavigationDefaultItemId) {
+            R.id.menu_item_learning -> R.drawable.outline_history_edu_24
+            R.id.menu_item_playground -> R.drawable.outline_playground_24px
+            else -> R.drawable.outline_home_24
+        }
+        val defaultBottomNavigationFragmentStateImageView = parentView.findViewById<ImageView>(R.id.settings_fragment_image_view_icon_default_fragment)
+        defaultBottomNavigationFragmentStateImageView.setImageResource(defaultBottomNavigationFragmentStateIcon)
+    }
+
     private fun loadAndShowDarkMode() {
         val darkModeDescriptionTextView = parentView.findViewById<TextView>(R.id.settings_fragment_text_view_description_night_mode)
-        val darkModeState = requireActivity()
-            .getSharedPreferences(GlobalVariables.PREFERENCES_NAME_SETTINGS, Activity.MODE_PRIVATE)
-            .getInt(GlobalVariables.PREFERENCES_SETTINGS_DARK_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-
+        val darkModeState = settingsPreferences.getInt(GlobalVariables.PREFERENCES_SETTINGS_DARK_MODE, AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
         when (darkModeState) {
             AppCompatDelegate.MODE_NIGHT_NO -> darkModeDescriptionTextView.text = getString(R.string.off)
             AppCompatDelegate.MODE_NIGHT_YES -> darkModeDescriptionTextView.text = getString(R.string.on)
             AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM -> darkModeDescriptionTextView.text = getString(
                 R.string.system_default
             )
+        }
+    }
+
+    private fun languageLabelToLocale(languageLabel: String): Locale {
+        Log.d(TAG, "languageLabelToLocale($languageLabel)")
+        return when(languageLabel) {
+            getString(R.string.arabic) -> Locale("ar")
+            getString(R.string.english) -> Locale("en")
+            getString(R.string.french) -> Locale("fr")
+            getString(R.string.german) -> Locale("de")
+            getString(R.string.italian) -> Locale("it")
+            getString(R.string.korean) -> Locale("ko")
+            getString(R.string.persian) -> Locale("fa")
+            getString(R.string.russia) -> Locale("ru")
+            getString(R.string.spanish) -> Locale("es")
+            else -> Locale("")
         }
     }
 
