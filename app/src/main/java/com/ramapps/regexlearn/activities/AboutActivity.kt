56439572145c58b09ced53b53023a15ec944ca29
RAM-tech400 <92999758+RAM-tech400@users.noreply.github.com>
2025-07-19 00:48:32 +0330

    Adding a button for going to the project github repo in the AboutActivity.

--- app/src/main/java/com/ramapps/regexlearn/activities/AboutActivity.kt
+++ app/src/main/java/com/ramapps/regexlearn/activities/AboutActivity.kt
@@ -1,20 +1,30 @@
 package com.ramapps.regexlearn.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ramapps.regexlearn.R
 import kotlin.math.max
+import androidx.core.net.toUri
 
 class AboutActivity : AppCompatActivity() {
+    private lateinit var goToAppGithubButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_about)
+        initViews()
         addListeners()
+    }
+
+    private fun initViews() {
+        goToAppGithubButton = findViewById(R.id.about_button_view_github)
     }
 
     private fun addListeners() {
@@ -28,6 +38,13 @@ class AboutActivity : AppCompatActivity() {
             v.setPadding(left, 0, right, 0)
 
             insets
+        }
+
+        goToAppGithubButton.setOnClickListener {
+            val repoUri = "https://github.com/RAM-tech400/RegExLearn.git".toUri()
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = repoUri
+            startActivity(intent)
         }
     }
 }
